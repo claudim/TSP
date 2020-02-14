6b9dc34208437bfd4771420e7b8ff84090579a58
ricman89 <onizuka-89@hotmail.it>
2020-02-14 10:14:00 +0100

    Added main file for testing approximation algorithm.

---
+++ testMain.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Riccardo on 14/02/2020.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "networkit/graph/Graph.hpp"
+#include "networkit/graph/DFS.hpp"
+#include "TSPLib/TSPGraphReader.h"
+#include "TSPLib/KruskalMST.h"
+#include "TSPLib/Checker.h"
+
+using namespace NetworKit;
+
+int main() {
+
+    return 0;
+
+}

90b4db82db099ad36e8dbede635e87a3ce002d90
claudim <claudimarco@hotmail.it>
2020-02-21 10:49:48 +0100

    modified testMain.cpp to improve readability

--- testMain.cpp
+++ testMain.cpp
@@ -15,16 +15,14 @@
 using namespace NetworKit;
 namespace fs = std::filesystem;
 
-const int OPERAZIONE = 1; // cambiare a seconda dell'operazione che si vuole eseguire
+const int OPERAZIONE = 3; // cambiare a seconda dell'operazione che si vuole eseguire
 
 std::map<std::string, edgeweight> loadBestCost()
 {
     std::map<std::string, edgeweight > tourBestCost;
     tourBestCost["a280"] = 2579;
     tourBestCost["pr1002"] = 259045 ;
-    //tourBestCost["ulysses16"] = 76 ; // controllare
     tourBestCost["berlin52"] = 7542;
-    //tourBestCost["eil101"] = 7542; // da calcolare il costo ottimo partendo dal tour
     tourBestCost["eil51"] = 426;
     tourBestCost["eil76"] = 538;
     tourBestCost["gr120"] = 6942;
@@ -42,8 +40,6 @@ std::map<std::string, edgeweight> loadBestCost()
     tourBestCost["fri26"] = 937;
     tourBestCost["gr17"] = 2085;
     tourBestCost["p15"] = 291;
-
-    //tourBestCost["gr96"] = 55209; // da controllare
     return tourBestCost;
 }
 
@@ -84,7 +80,7 @@ double stdDeviationComputation(Graph &g, double mean) {
 
 double meanComputation(Graph &g){
     double sum = 0.0, mean=0.0;
-    g.forEdges([&](node u, node v, edgeweight w) { sum += g.weight(u, v); });
+    g.forEdges([&](node u, node v, edgeweight w)  { sum += g.weight(u, v); });
     return mean = sum / g.numberOfEdges();
 
 }
@@ -129,12 +125,10 @@ int main() {
     TSPGraphReader gr = TSPGraphReader();
     TSPGraphMaker tspGraph = TSPGraphMaker();
     double averageRatio = 0, averageRatioMST=0, sum = 0, sumMST = 0;
-    //std::cout<< "averageRatio: "<<averageRatio<<std::endl;
     for (const auto &item : fs::directory_iterator(path)) {
         if(item.path().extension() == ".edges") {
             std::vector<node> H;
             Graph g = gr.getGraph(item.path());
-            //Graph g = gr.getGraph(path);
             if(Checker::isComplete(g) && Checker::isNonNegativeWeights(g))
             {
                 switch (OPERAZIONE) {
@@ -151,9 +145,9 @@ int main() {
                         writeOnCSVFileMeanVariance(item.path().filename().stem(), mean, stdDeviation, uniformDistributionMean,uniformDistributionStdDeviation, min, max, meanDifference, stdDeviationDifference);
                         break;
                     }
-                    case 1:
+                    case 1: // compute graph weight frequency
                     {
-                        double max =  getGraphMaxWeight(g);
+                        //double max =  getGraphMaxWeight(g);
                         std::map<edgeweight , int > A;
                         g.forEdges([&](node u, node v, edgeweight w) {
                             A[w] = A[w]+1;
@@ -163,7 +157,8 @@ int main() {
                         }
                         break;
                     }
-                    default: // esegui algoritmo
+                    case 2: // execute algorithm and find ratio and average
+                    {
                         H = ApproximationTSPAlgorithm().run(g);
                         Graph tspG = tspGraph.findTSPGraph(g, H);
                         // insert a tour approx cost
@@ -173,32 +168,47 @@ int main() {
                         mst.forEdges(
                                 [&](node u, node v, edgeweight w) { mstCostValue = mstCostValue + g.weight(u, v); });
                         mstCost[item.path().filename().stem()] = mstCostValue;
+                        for (std::map<std::string, edgeweight>::value_type &x : tourApproxCost) {
+                            std::cout.precision(5);
+                            //double ratio  = tourBestCost[x.first] / x.second;
+                            double ratio = double(x.second) / tourBestCost[x.first];
+                            double ratioMST = double(x.second) / mstCost[x.first];
+                            std::cout << "costobest " << tourBestCost[x.first] << std::endl;
+                            std::cout << "costoapprox " << x.second << std::endl;
+                            std::cout << std::fixed << "ratio " << x.first << " :" << ratio << std::endl;
+                            sum = sum + ratio;
+                            sumMST = sumMST + ratioMST;
+                            writeOnCSVFile(x.first, mstCost[x.first], x.second, tourBestCost[x.first], ratio, ratioMST);
+
+                        }
+                        averageRatio = sum / tourApproxCost.size();
+                        averageRatioMST = sumMST / tourApproxCost.size();
+
+                        std::cout << "average: " << averageRatio << std::endl;
+                        std::cout << "average MST: " << averageRatioMST << std::endl;
                         break;
+                    }
+                    default: //execute algorithm
+                    {
+                        H = ApproximationTSPAlgorithm().run(g);
+
+                        TSPGraphMaker tspGraph = TSPGraphMaker();
+                        Graph tsp = tspGraph.findTSPGraph(g,H);
+
+                        std::cout << "Tour: " ;
+
+                        for (auto i = H.begin(); i != H.end(); ++i)
+                            std::cout << *i << ' ';
+                        std::cout << std::endl << "Tour cost: " << tspGraph.getTSPCost()<< std::endl;
+
+                        break;
+                    }
                 }
 
             }
         }
     }
-    /*for (std::map<std::string, edgeweight >::value_type &x : tourApproxCost) {
-        std::cout.precision(5);
-        //double ratio  = tourBestCost[x.first] / x.second;
-        double ratio = double(x.second) / tourBestCost[x.first];
-        double ratioMST = double(x.second) / mstCost[x.first];
-        std::cout << "costobest " << tourBestCost[x.first] << std::endl;
-        std::cout << "costoapprox " << x.second << std::endl;
-        std::cout << std::fixed << "ratio " << x.first << " :" << ratio << std::endl;
-        sum = sum + ratio;
-        sumMST = sumMST + ratioMST;
-        writeOnCSVFile(x.first,mstCost[x.first],x.second,tourBestCost[x.first],ratio, ratioMST);
-
-    }
-    averageRatio = sum / tourApproxCost.size();
-    averageRatioMST = sumMST / tourApproxCost.size();
-
-    std::cout << "average: " << averageRatio << std::endl;
-    std::cout << "average MST: " << averageRatioMST << std::endl;
-     */
-
+    
     return 0;
 
 }
